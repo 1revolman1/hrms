a381d68937f4b53785626535e8c0ca0c5ec68b2b
1revolman1 <48256429+1revolman1@users.noreply.github.com>
2020-06-10 16:19:33 +0300

    after  pulling

--- src/pages/CV/CV.jsx
+++ src/pages/CV/CV.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import background from "../../assets/img/bg.png";
+// import background from "../../assets/img/bg.png";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import Header from "./Header/Header";
 const StyledCV = styled.div``;
 function CV(props) {
   const { id } = props.match.params;
   return (
     <StyledCV>
+      <Header />
       Сотрудник {id} !<Link to="/">Обратно</Link>
     </StyledCV>
   );
--- src/pages/CV/Header/Header.jsx
+++ src/pages/CV/Header/Header.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import background from "../../../assets/img/bg.png";
 
-const StyledHeader = styled.div``;
+const StyledHeader = styled.div`
+  background-image: url(${background});
+`;
 
 function Header(props) {
-  return <StyledHeader></StyledHeader>;
+  return (
+    <StyledHeader>
+      <div>
+        <div className="img">
+          <img
+            src="https://s.dou.ua/img/avatars/200x200_dscn0093-2.jpg"
+            alt="Avatar of user"
+          />
+        </div>
+        <div className="text">
+          <h3>Шестопалов Владимир</h3>
+          <h4>
+            Sales Manager <span>28</span>
+          </h4>
+        </div>
+      </div>
+      <div>
+        <button>Скачать резюме</button>
+        <button>Редактировать профиль</button>
+      </div>
+    </StyledHeader>
+  );
 }
 
 export default Header;
