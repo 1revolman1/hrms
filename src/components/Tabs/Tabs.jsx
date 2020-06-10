47f87a5ebdd0189a01077a5737e2bbdad78bfaa4
rbkv-dev <rbkv.dev@gmail.com>
2020-06-10 09:44:31 +0300

    make login page better

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -3,49 +3,62 @@ import styled from "styled-components";
 import "./Tabs.scss";
 const StyledTabsWrap = styled.div`
   margin: 0 auto;
-  /* .tabsBlock {
-    display: flex;
-    & > div {
-      padding: 8px 28px;
-      font-family: "Montserrat", "sans-serif";
-      font-size: 18px;
-      font-weight: 500;
-      font-stretch: normal;
-      font-style: normal;
-      line-height: normal;
-      letter-spacing: 0.9px;
-      border-radius: 7px;
-      color: #595959;
-      &[data-active="true"] {
-        background-color: #c74e4e;
-        color: #ffffff;
-      }
-      &[data-active="false"] {
-        background-color: #e5e5e5;
-        color: #595959;
-      }
-      &:first-of-type {
-        margin-left: auto;
-      }
-      &:last-of-type {
-        margin-right: auto;
-      }
-    }
-  } */
+  width: ${props => props.wdth}px;
+  nav {
+    border-radius: 7px;
+    background-color: #e5e5e5;
+  }
+`;
+const StyledTab = styled.div`
+  border-radius: 7px;
+  line-height: 50px;
+  height: 100%;
+  font-size: 15px;
+  display: inline-block;
+  position: relative;
+  z-index: 1;
+  text-decoration: none;
+  text-transform: uppercase;
+  text-align: center;
+  color: white;
+  &[data-active="true"] {
+    background-color: #c74e4e;
+  }
+
+  width: ${props => props.wdth}px;
+`;
+const StyledTabAnimation = styled.div`
+
+  position: absolute;
+  height: 100%;
+  top: 0;
+  z-index: 0;
+  transition: all 0.5s ease 0s;
+  border-radius: 8px;
+
+  width: ${props => props.wdth}px;
+  left: ${props => props.left}px;
+  background-color: #1abc9c;
+
 `;
 
-function Tabs() {
+
+function Tabs(props) {
   //   const [fetch, setFetch] = useState();
   console.log("RENDER TABS");
+  // const wdth = useState(150);
+  // const left = useState(0);
+
   return (
-    <StyledTabsWrap>
+    <StyledTabsWrap wdth="600">
       <nav>
-        <div data-active="true">Активные</div>
-        <div data-active="false">Неактивные</div>
+        <StyledTab data-active="false" wdth="200" onclick={console.log('123')}>Активные</StyledTab>
+        <StyledTab data-active="true" wdth="200">Неактивные</StyledTab>
+        <StyledTab data-active="false" wdth="200" >Активные</StyledTab>
         {/* <div data-active="false">Неактивные</div>
         <div data-active="false">Неактивные</div>
         <div data-active="false">Активные</div> */}
-        <div class="animation start-home"></div>
+        <StyledTabAnimation class="animation start-home" wdth={props.wdth} left="100"></StyledTabAnimation>
       </nav>
       {/* <div className="tabsBlock">
         <div data-active="true">Активные</div>
